822309d7ecbfbdf716f643535f35ec81a1213b70
Hanwoong <gkswk117@gmail.com>
2023-07-01 20:46:33 +0900

    [타입스크립트로 블록체인 만들기] #2.3 Types of TS part Two(11:16) 완료.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+//#2.1 Implicit Types vs Explicit Types
+let str = "hello" //Implicit Types -> better
+let strr : string = "world" //Explicit Types
+
+//#2.2 Types of TS part One
 type Player={
-    name:string,
+    readonly name:string,
     age?:number
 }
 const nico : Player={
@@ -10,15 +15,43 @@ const lynn : Player={
     age:28
 }
 
-function playerMaker(name:string) : Player {
+function PlayerMaker(name:string) : Player {
     return {
         name:name
     }
 }
+const PlayerMaker2 = (name:string) : Player => ({name:name})
 //함수의 argument의 타입 지정, 함수의 return값의 타입 지정.
 //argument로 string 타입을 받고, return으로 Player 타입을 반환한다.
-const hanwoong = playerMaker("hw")
+
+const hanwoong = PlayerMaker("hw")
 hanwoong.age = 32
 console.log(hanwoong)
 
-const playerMaker2 = (name:string) : Player => ({name:name})
+//#2.3 Types of TS part Two
+//Readonly
+//hanwoong.name ="rlahWl"
+
+const numberArray: readonly number[] = [1,2,3,4]
+//numberArray.push(1)
+
+//Tuple
+const tupleArray: [string, number, boolean] = ["nico", 1, false]
+tupleArray.push("las")
+// console.log(tupleArray[3])
+// 이럴 때는 아래와 같이 하면 됨.
+const numberArr: (number|string)[] = [1, "nico", 2]
+numberArr.push(3)
+console.log(numberArr[3])
+
+const a = [1,2,3,4]
+const b = true
+//const c = a+b
+
+//Any
+const d:any[] = [1,2,3,4]
+const e:any = true
+const f = d+e
+console.log(f)
+d.push(e)
+console.log(d)
