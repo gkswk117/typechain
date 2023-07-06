e0e2dc38133d1f9172c0d063cb16bf0f707a3c3c
POSTECH\gkswk117 <gkswk117@gmail.com>
2023-07-06 22:43:36 +0900

    .

--- src/#3.1 Overloading.ts
+++ src/#3.1 Overloading.ts
@@ -14,6 +14,7 @@ type Push = {
 const testFunc :Push = (config) => {
   // I don't know why this line raises small errors in vscode.
   // https://www.typescriptlang.org/play?#code/Q에서는 에러 안 뜸.
+  // => tsconfig.json에서 "strict"를 true로 해주면 에러가 안 뜸.
   if(typeof config==="string"){console.log(config)}
   else{
     console.log(config.path)
